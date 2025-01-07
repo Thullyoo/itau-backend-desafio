ca283ed1f1d9a00b9e5a8d177cd1c82bc568b463
Thullyoo <thullyofilho@gmail.com>
2025-01-07 14:22:03 -0300

    Initial commit

---
+++ src/main/java/br/thullyoo/itau_backend_desafio/ItauBackendDesafioApplication.java
@@ -0,0 +1,13 @@
+package br.thullyoo.itau_backend_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauBackendDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauBackendDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/thullyoo/itau_backend_desafio/ItauBackendDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.thullyoo.itau_backend_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauBackendDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
