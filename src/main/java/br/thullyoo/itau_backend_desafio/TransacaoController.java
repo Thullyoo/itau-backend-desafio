3e538c6d350d2959b47dd6b9f203312d1a0d5170
Thullyoo <thullyofilho@gmail.com>
2025-01-07 15:15:43 -0300

    feat: add Transacao Controller and endpoint to register Transacao

---
+++ src/main/java/br/thullyoo/itau_backend_desafio/TransacaoController.java
@@ -0,0 +1,35 @@
+package br.thullyoo.itau_backend_desafio;
+
+import br.thullyoo.itau_backend_desafio.DTO.TransacaoRequest;
+import br.thullyoo.itau_backend_desafio.service.TransacaoService;
+import org.springframework.beans.NotReadablePropertyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+
+@RestController
+public class TransacaoController {
+
+    private final TransacaoService transacaoService;
+
+    public TransacaoController(TransacaoService transacaoService) {
+        this.transacaoService = transacaoService;
+    }
+
+    @PostMapping(value = "/transacao")
+    public ResponseEntity<Void> registrarTransacao(@RequestBody TransacaoRequest transacaoRequest){
+        try{
+            transacaoService.registrarTransacao(transacaoRequest);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.unprocessableEntity().build();
+        } catch (Exception e){
+            return ResponseEntity.badRequest().build();
+        }
+
+    }
+
+}
