28bf7f8292d57268fd2ae463a4fc17f6dc80e5ac
Thullyoo <thullyofilho@gmail.com>
2025-01-10 13:12:04 -0300

    feat: add endpoint to delete all transacoes

--- src/main/java/br/thullyoo/itau_backend_desafio/controller/TransacaoController.java
+++ src/main/java/br/thullyoo/itau_backend_desafio/controller/TransacaoController.java
@@ -4,6 +4,7 @@ import br.thullyoo.itau_backend_desafio.DTO.TransacaoRequest;
 import br.thullyoo.itau_backend_desafio.service.TransacaoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,4 +31,9 @@ public class TransacaoController {
 
     }
 
+    @DeleteMapping(value = "/transacao")
+    public ResponseEntity<Void> deletarTransacoes(){
+        transacaoService.deletarTransacoes();
+        return ResponseEntity.ok(null);
+    }
 }
