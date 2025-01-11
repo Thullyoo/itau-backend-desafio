ea9cda180ed346acbf30b02c48cbe8be892d3e60
Thullyoo <thullyofilho@gmail.com>
2025-01-11 15:53:23 -0300

    feat: add documentation on estatistica controller

--- src/main/java/br/thullyoo/itau_backend_desafio/controller/EstatisticaController.java
+++ src/main/java/br/thullyoo/itau_backend_desafio/controller/EstatisticaController.java
@@ -3,11 +3,16 @@ package br.thullyoo.itau_backend_desafio.controller;
 import br.thullyoo.itau_backend_desafio.DTO.EstatisticaDTO;
 import br.thullyoo.itau_backend_desafio.repository.TransacaoRepository;
 import br.thullyoo.itau_backend_desafio.service.EstatisticaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Estatística Controller")
 public class EstatisticaController {
 
     private final EstatisticaService estatisticaService;
@@ -16,6 +21,10 @@ public class EstatisticaController {
         this.estatisticaService = estatisticaService;
     }
 
+    @Operation(summary = "Registrar transação", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Estátistica gerada com sucesso!"),
+    })
     @GetMapping(value = "estatistica")
     public ResponseEntity<EstatisticaDTO> gerarEstatistica(){
         return ResponseEntity.ok(estatisticaService.gerarEstatistica());
