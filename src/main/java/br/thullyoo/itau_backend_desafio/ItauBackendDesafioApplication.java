5675ea233be439aae637998dcff4af8fdb428abc
Thullyoo <thullyofilho@gmail.com>
2025-01-11 15:48:05 -0300

    feat: add tittle, version and description on swagger

--- src/main/java/br/thullyoo/itau_backend_desafio/ItauBackendDesafioApplication.java
+++ src/main/java/br/thullyoo/itau_backend_desafio/ItauBackendDesafioApplication.java
@@ -1,9 +1,12 @@
 package br.thullyoo.itau_backend_desafio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Desafio Itáu Back-end", version = "1.0", description = "API para salvar Transações e gerar Estatísticas"))
 public class ItauBackendDesafioApplication {
 
 	public static void main(String[] args) {
